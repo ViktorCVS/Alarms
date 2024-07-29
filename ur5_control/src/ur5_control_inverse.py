6acce3a5e683cee851cc49e3b119a2ff85c676d8
Victor Coutinho Vieira Santos <69547580+ViktorCVS@users.noreply.github.com>
2024-07-29 00:08:59 -0300

    Update ur5_control_inverse.py

--- ur5_control/src/ur5_control_inverse.py
+++ ur5_control/src/ur5_control_inverse.py
@@ -7,8 +7,10 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint        # J
 
 import random                                                                # random para escolher aleatoriamente um ângulo para cada junta.
 import numpy as np                                                           # numpy para trabalho matricial.
-from math import sin,cos,pi, ceil, inf                                       # math para uso das funções seno, cosseno e teto, além do valor de pi e infinito.
+from math import sin,cos,pi, ceil, inf, acos, asin, atan2, sqrt                                     # math para uso das funções seno, cosseno e teto, além do valor de pi e infinito.
 from tf.transformations import euler_from_quaternion                         # euler_from_quaternion para converter a orientação de quaternion para euler.
+from scipy.spatial.transform import Rotation as R
+
 
 class CompareTrajectory():
     def __init__(self):
@@ -34,16 +36,28 @@ class CompareTrajectory():
         # Vetor que acumulará a posição prevista para a junta, posição passada e atual.
         self.poses = [[0,0,0],0]
 
+        self.orien = [[0,0,0],0]
+
         # Variáveis auxiliares para caracterização do erro.
         self.erro_max = -inf
         self.erro_min = inf
         self.erro_medio = 0
 
-        #
-        self.margem = 85
+        self.erro_max_orien = -inf
+        self.erro_min_orien = inf
+        self.erro_medio_orien = 0
 
         # Contador de iteração.
         self.contador = 0
+
+        self.j1 = 0
+        self.j2 = 0
+        self.j3 = 0
+        self.j4 = 0
+        self.j5 = 0
+        self.j6 = 0
+
+        self.J = np.zeros((6,2))
 
         # Timer para enviar a referência de posição periodicamente.
         rospy.Timer(rospy.Duration(tempo_referencia), self.timer_callback)  
@@ -63,12 +77,45 @@ class CompareTrajectory():
         # Iniciando a mensagem de ponto de referência.
         point = JointTrajectoryPoint()
 
+        # ----- Escolhendo aleatóriamente ângulo para as juntas, respeitando as limitações físicas para que não haja colisão/autocolisão. -----
+
+        self.j1 = random.uniform(-pi,pi)
+        self.j2 = random.uniform(0,-pi)
+
+        if self.j2 > -pi/2:
+            self.j3 = random.uniform(0,-pi/2)
+        elif self.j2 < -pi/2:
+            self.j3 = random.uniform(0,pi/2)
+
+        self.j4 = random.uniform(0,-pi)
+
+        self.j5 = random.uniform(-pi,pi)
+
+        self.j6 = random.uniform(-pi,pi)
+
+
+        self.J[0,1] = self.j1
+        self.J[2,1] = self.j3
+        self.J[4,1] = self.j5
+        self.J[5,1] = self.j6
+
+        # -------------------------------------------------------------------------------------------------------------------------------------
+
+        # Montando a mensagem do ponto de referência com base no valor das juntas.
+        point.positions = [self.j1, self.j2, self.j3, self.j4, self.j5, self.j6]
+
+        # Definindo o tempo para que a trajetória seja realizada
+        point.time_from_start = rospy.Duration(self.tempo_trajetoria)
+
+        # Adicionando a mensagem de ponto para a mensagem da trajetória.
+        traj.points.append(point)
+
         # ----- Definindo as medidas estruturais do robô UR5, considerando ajustes de simulação e o sistema de coordenadas. -----
 
         d1 = 0.08915895487108214+0.1000000368892521
 
-        a2  = 0.424994
-        d2 = -0.002161932
+        a2 = 0.424994
+        d2 = 0.002161932
 
         a3 = 0.392236
         d3 = 0.109051
@@ -77,64 +124,111 @@ class CompareTrajectory():
 
         d4 = 0.09464348
 
-        dx = 0.001118318
+        ax = 0.001354042
 
         d5 = 0.0823 
 
         # ----------------------------------------------------------------------------------------------------------------------- 
 
 
-        # ----- Escolhendo aleatóriamente uma posição para o executor, respeitando as limitações físicas para que não haja colisão/autocolisão. -----
-
-        max_amplitude = (a2+a3)*self.margem/100
-
-        while True:
-
-            x_executor = random.uniform(-max_amplitude,max_amplitude)
-            y_executor = random.uniform(-max_amplitude,max_amplitude)
-            z_executor = random.uniform(-max_amplitude,max_amplitude)
-
-            if x_executor**2+y_executor**2+z_executor**2 < max_amplitude**2:
-
-                break
-
-        # -------------------------------------------------------------------------------------------------------------------------------------------
-
-        # Montando a mensagem do ponto de referência com base no valor das juntas.
-        point.positions = [j1, j2, j3, j4, j5, j6]
-
-        # Definindo o tempo para que a trajetória seja realizada
-        point.time_from_start = rospy.Duration(self.tempo_trajetoria)
-
-        # Adicionando a mensagem de ponto para a mensagem da trajetória.
-        traj.points.append(point)
-
-
-
-
-
         # Montando as medidas estruturais como lista para automação.
-        d =    [d1 ,0    ,d2  ,d3   ,d4+de ,d5   ]
-        a =    [0  ,0    ,-a2 ,-a3  ,dx    ,0    ]
-        alfa = [0  ,pi/2 ,0   ,0    ,pi/2  ,-pi/2]
-        j =    [j1 ,j2   ,j3  ,j4   ,j5    ,j6   ]
+        d =    [d1 ,0    ,-d2 ,d3    ,d4+de ,d5   ]
+        a =    [0  ,0    ,-a2 ,-a3+ax,0     ,0    ]
+        alfa = [0  ,pi/2 ,0   ,0     ,pi/2  ,-pi/2]
+        j =    [self.j1 ,self.j2   ,self.j3  ,self.j4    ,self.j5    ,self.j6   ]
+
+        if self.orientation[2] > 0: alfa = self.orientation[2]-pi
+        elif self.orientation[2] < 0: alfa = self.orientation[2]+pi
+
+        T = np.matrix([[cos(self.orientation[2])*cos(self.orientation[1]), cos(self.orientation[2])*sin(self.orientation[1])*sin(self.orientation[0])-sin(self.orientation[2])*cos(self.orientation[0]), cos(self.orientation[2])*sin(self.orientation[1])*cos(self.orientation[0])+sin(self.orientation[2])*sin(self.orientation[0]), self.position[0]],
+                       [sin(self.orientation[2])*cos(self.orientation[1]), sin(self.orientation[2])*sin(self.orientation[1])*sin(self.orientation[0])+cos(self.orientation[2])*cos(self.orientation[0]), sin(self.orientation[2])*sin(self.orientation[1])*cos(self.orientation[0])-cos(self.orientation[2])*sin(self.orientation[0]), self.position[1]],
+                       [-sin(self.orientation[1]), cos(self.orientation[1])*sin(self.orientation[0]), cos(self.orientation[1])*cos(self.orientation[0]), self.position[2]],
+                       [0, 0, 0, 1]])
+
+        T_5 = T*np.matrix([[0],[0],[-d5],[1]])
+
+        P_5 = [T_5[0,0],T_5[1,0],T_5[2,0]]
+
+        tetha_1 = atan2(P_5[1],P_5[0])+acos(d3/sqrt(P_5[0]**2+P_5[1]**2))+pi/2
+        tetha_1x = atan2(P_5[1],P_5[0])-acos(d3/sqrt(P_5[0]**2+P_5[1]**2))+pi/2
+
+        if tetha_1 > 0: 
+            tetha_1 -= pi 
+        elif tetha_1 < 0: 
+            tetha_1 += pi
+
+        if tetha_1x > 0: 
+            tetha_1x -= pi 
+        elif tetha_1x < 0: 
+            tetha_1x += pi
+
+        if abs(self.J[0,0]-tetha_1)>=abs(self.J[0,0]-tetha_1x):
+            tetha_1 = tetha_1x
+
+        rex = (-self.position[0]*sin(tetha_1)+self.position[1]*cos(tetha_1)-d3)/d5
+        tetha_5 = acos(rex)
+
+        if abs(tetha_5 - self.J[4,0]) >= abs(-tetha_5 -self.J[4,0]):
+            tetha_5 *= -1
+
+        print("tetha 1R: ",self.J[0,0])
+        print("tetha 1E: ",tetha_1)
+
+        print()
+        print("tetha 5R: ",self.J[4,0])
+        print("tetha 5E: ",tetha_5)
+        print()
 
 
-        # Laço de repetição para multiplicar cada T_i e encontrar a transformação final T com os valores das medidas estruturais e ângulos de cada junta
-        i=0
-        T=1
-        while(i<len(d)):
-            T *= self.T_link(j[i],alfa[i],a[i],d[i])
-            i+=1
-    
+
+        print("tetha 6R: ",self.J[5,0])
+
+        if self.orientation[2] > 0: alfa = self.orientation[2]-pi
+        elif self.orientation[2] < 0: alfa = self.orientation[2]+pi
+
+        T = np.matrix([[cos(alfa)*cos(self.orientation[1]), cos(alfa)*sin(self.orientation[1])*sin(self.orientation[0])-sin(alfa)*cos(self.orientation[0]), cos(alfa)*sin(self.orientation[1])*cos(self.orientation[0])+sin(alfa)*sin(self.orientation[0]), -self.position[0]],
+                       [sin(alfa)*cos(self.orientation[1]), sin(alfa)*sin(self.orientation[1])*sin(self.orientation[0])+cos(alfa)*cos(self.orientation[0]), sin(alfa)*sin(self.orientation[1])*cos(self.orientation[0])-cos(alfa)*sin(self.orientation[0]), -self.position[1]],
+                       [-sin(self.orientation[1]), cos(self.orientation[1])*sin(self.orientation[0]), cos(self.orientation[1])*cos(self.orientation[0]), self.position[2]],
+                       [0, 0, 0, 1]])
+        T_i = np.linalg.inv(T)
+
+        tetha_6 = atan2((-T_i[1,0]*sin(tetha_1)+T_i[1,1]*cos(tetha_1))/sin(tetha_5),(T_i[0,0]*sin(tetha_1)-T_i[0,1]*cos(tetha_1))/sin(tetha_5))
+        print("tetha 6E: ",tetha_6)
+
+        T*
+
+        print()
+        print("tetha 3R: ",self.J[2,0])
+        print("tetha 3E: ",tetha_3)
+
+
+
+        self.J[0,0] = self.J[0,1]
+        self.J[2,0] = self.J[2,1]
+        self.J[4,0] = self.J[4,1]
+        self.J[5,0] = self.J[5,1]
+
         # Encontrando a nova posição pela quarta coluna e três primeiras linhas da matrix T.
         new_position = [-T[0,3],-T[1,3],T[2,3]]
+        
+        gamma = atan2(T[2,1],T[2,2])
+        alfa = atan2(T[1,0],T[0,0])
+        sB = -T[2,0]
+        cB = T[0,0]/cos(alfa)
+        beta = atan2(sB,cB)
+
+        if alfa > 0: alfa -= pi
+        elif alfa <= 0: alfa += pi
+        
+        new_orientation = [gamma,beta,alfa]
 
         # Nova pose para previsão atual.
-        self.poses[1] = new_position
+        self.poses[0] = new_position
+        self.orien[0] = new_orientation
 
         # Calculando o erro entre o ground truth e o cálculo matricial.
         Erro = [100*abs(self.position[0]-self.poses[0][0]),100*abs(self.position[1]-self.poses[0][1]),100*abs(self.position[2]-self.poses[0][2])]
+        Erro_orien = [100*abs(self.orientation[0]-self.orien[0][0]),100*abs(self.orientation[1]-self.orien[0][1]),100*abs(self.orientation[2]-self.orien[0][2])]
 
         # Formatação das mensagens no terminal, indicando o número da iteração, posição pelo grounth truth e pelo cálculo matricial, além do erro
         # e o valor máximo que esse erro chegou em % discreta de 0.5 em 0.5%.
@@ -142,27 +236,49 @@ class CompareTrajectory():
             
             print("-------------------------------------------------------------------------")
             print(f"Iteração número: {self.contador}\n")
-            print(f"Posição [DH]:\nx: {self.poses[0][0]}\ny: {self.poses[0][1]}\nz: {self.poses[0][2]}\n")       #print("pos[k+1]: ",new_position)
-            print(f"Posição [GT]:\nx: {self.position[0]}\ny: {self.position[1]}\nz: {self.position[2]}\n")
-            print(f"Erro:\nx: {Erro[0]}\ny: {Erro[1]}\nz: {Erro[2]}\n")
+            # print(f"Posição [DH]:\nx: {self.poses[0][0]}\ny: {self.poses[0][1]}\nz: {self.poses[0][2]}\n")
+            # print(f"Orientação [DH]:\nx: {self.orien[0][0]}\ny: {self.orien[0][1]}\nz: {self.orien[0][2]}\n")
+            # print(f"Posição [GT]:\nx: {self.position[0]}\ny: {self.position[1]}\nz: {self.position[2]}\n")
+            # print(f"Orientação [GT]:\nx: {self.orientation[0]}\ny: {self.orientation[1]}\nz: {self.orientation[2]}\n")
+            # print(f"Erro de posição:\nx: {Erro[0]}%\ny: {Erro[1]}%\nz: {Erro[2]}%\n")
+            # print(f"Erro de orientação:\nx: {Erro_orien[0]}%\ny: {Erro_orien[1]}%\nz: {Erro_orien[2]}%\n")
 
-            limite = ceil(max(Erro))
+            # limite = ceil(max(Erro))
 
-            if(limite - max(Erro) > 0.5):
-                limite -= 0.5
+            # if(limite - max(Erro) > 0.5):
+            #     limite -= 0.5
 
-            if Erro[0]<limite and Erro[1]<limite and Erro[2]<limite:
-                print(f"Todos os erros absolutos inferiores a {limite}%")
-            if max(Erro)>self.erro_max:
-                self.erro_max = max(Erro)
-            if min(Erro)<self.erro_min:
-                self.erro_min = min(Erro)
-            self.erro_medio += (Erro[0]+Erro[1]+Erro[2])/3
+            # if Erro[0]<limite and Erro[1]<limite and Erro[2]<limite:
+            #     print(f"Todos os erros absolutos de posição inferiores a {limite}%")
+            # if max(Erro)>self.erro_max:
+            #     self.erro_max = max(Erro)
+            # if min(Erro)<self.erro_min:
+            #     self.erro_min = min(Erro)
+            # self.erro_medio += (Erro[0]+Erro[1]+Erro[2])/3
 
-            print(f"Erro máximo: {self.erro_max}%")
-            print(f"Erro mínimo: {self.erro_min}%")
-            print(f"Erro médio: {self.erro_medio/self.contador}%")
-            
+            # print(f"Erro máximo de posição: {self.erro_max}%")
+            # print(f"Erro mínimo de posição: {self.erro_min}%")
+            # print(f"Erro médio de posição: {self.erro_medio/self.contador}%")
+
+            # print()
+
+            # limite_orien = ceil(max(Erro_orien))
+
+            # if(limite_orien - max(Erro_orien) > 0.5):
+            #     limite_orien -= 0.5
+
+            # if Erro_orien[0]<limite_orien and Erro_orien[1]<limite_orien and Erro_orien[2]<limite_orien:
+            #     print(f"Todos os erros absolutos de orientação inferiores a {limite_orien}%")
+            # if max(Erro_orien)>self.erro_max_orien:
+            #     self.erro_max_orien = max(Erro_orien)
+            # if min(Erro_orien)<self.erro_min_orien:
+            #     self.erro_min_orien = min(Erro_orien)
+            # self.erro_medio_orien += (Erro_orien[0]+Erro_orien[1]+Erro_orien[2])/3
+
+            # print(f"Erro máximo de orientação: {self.erro_max_orien}%")
+            # print(f"Erro mínimo de orientação: {self.erro_min_orien}%")
+            # print(f"Erro médio de orientação: {self.erro_medio_orien/self.contador}%")
+
             print()
 
         # Enviando a trajetória efetivamente.
@@ -170,6 +286,7 @@ class CompareTrajectory():
 
         # A nova posição se torna antiga.
         self.poses[0] = self.poses[1]
+        self.orien[0] = self.orien[1]
 
         # Incrementando o contador.
         self.contador +=1
@@ -185,6 +302,13 @@ class CompareTrajectory():
         orientation = msg.pose[-1].orientation
         orientation = [orientation.x,orientation.y,orientation.z,orientation.w]
         self.orientation = list(euler_from_quaternion(orientation))
+
+        position = msg.pose[-2].position
+        self.position_link = [position.x,position.y,position.z]
+
+        orientation = msg.pose[-2].orientation
+        orientation = [orientation.x,orientation.y,orientation.z,orientation.w]
+        self.orientation_link = list(euler_from_quaternion(orientation))
 
     def T_link(self,theta,alfa,a,d):
 
